cdc0c622ed9439931ec1493279ea8974bf0819d5
Ben J. Smith <benjsmith@gmail.com>
2024-06-09 17:43:02 -0700

    deploy php

---
+++ vid.php
@@ -0,0 +1,53 @@
+<html>
+    <body>
+    <?php
+// index.php
+
+// Check if the 'test' GET parameter is set
+if (isset($_GET['vidid'])) {
+    // Retrieve the value of the 'test' parameter
+    $vidid = $_GET['vidid'];
+
+    // Sanitize the input to prevent XSS
+    $vidid = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
+
+    // Display the value
+    echo "<h1>The value of the 'test' parameter is: $vidid</h1>";
+} else {
+    echo "<h1>No 'test' parameter provided!</h1>";
+}
+
+if (isset($_GET['username'])) {
+    // Retrieve the value of the 'test' parameter
+    $username = $_GET['username'];
+
+    // Sanitize the input to prevent XSS
+    $username = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
+
+    // Display the value
+    echo "<h1>The value of the 'userid' parameter is: $username</h1>";
+} else {
+    echo "<h1>No 'userid' parameter provided!</h1>";
+}
+
+?>
+
+	
+
+    testTikTok2.html
+    This is test tiktok 2
+    <br />
+    
+
+        <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@<?php echo $username; >/video/<?php echo $vidid; >" data-video-id="<?php echo $vidid; >" style="max-width: 605px;min-width: 325px;" > 
+        
+        	<section> 
+        		<a target="_blank" title="@shortfilm.mp4" href="https://www.tiktok.com/@<?php echo $username; >?refer=embed">@<?php echo $username; ></a> Never judge a book by its cover part 1 
+        		
+        	</section> 
+        
+        </blockquote> 
+        <script  src="https://www.tiktok.com/embed.js"></script>
+
+    </body>
+</html>
