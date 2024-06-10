99a72b849c1dc0e1ab5d967b71ef8731f7672ea1
Ben J. Smith <benjsmith@gmail.com>
2024-06-09 17:46:06 -0700

    fix php issues

--- vid.php
+++ vid.php
@@ -9,7 +9,7 @@ if (isset($_GET['vidid'])) {
     $vidid = $_GET['vidid'];
 
     // Sanitize the input to prevent XSS
-    $vidid = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
+    $vidid = htmlspecialchars($vidid, ENT_QUOTES, 'UTF-8');
 
     // Display the value
     echo "<h1>The value of the 'test' parameter is: $vidid</h1>";
@@ -39,10 +39,10 @@ if (isset($_GET['username'])) {
     <br />
     
 
-        <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@<?php echo $username; >/video/<?php echo $vidid; >" data-video-id="<?php echo $vidid; >" style="max-width: 605px;min-width: 325px;" > 
+        <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@<?php echo $username; ?>/video/<?php echo $vidid; ?>" data-video-id="<?php echo $vidid; ?>" style="max-width: 605px;min-width: 325px;" > 
         
         	<section> 
-        		<a target="_blank" title="@shortfilm.mp4" href="https://www.tiktok.com/@<?php echo $username; >?refer=embed">@<?php echo $username; ></a> Never judge a book by its cover part 1 
+        		<a target="_blank" title="@shortfilm.mp4" href="https://www.tiktok.com/@<?php echo $username; ?>?refer=embed">@<?php echo $username; ?></a> Video Title
         		
         	</section> 
         
